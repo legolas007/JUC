f73ffda51047b693d85d8d0d11c0ddf9d24c8a53
legolas007 <oukuanou@163.com>
2018-05-19 23:00:59 +0800

    自定义注解

--- src/main/java/com/usher/juc/annoations/NotThreadSafe.java
+++ src/main/java/com/usher/juc/annoations/NotThreadSafe.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)//指定该策略的Annotation可以修饰类，接口或枚举定义
 @Retention(RetentionPolicy.SOURCE)//注解生存期，Annotation值保留在源代码，编译器直接丢弃这种Annotation
-public @interface ThreadSafe {
+public @interface NotThreadSafe {
     String value() default "";
 }
